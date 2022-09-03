cbd61bc009d10880f1a320ef28369930b744eb6c
bia <beatrizlobo1998@gmail.com>
2022-09-03 19:41:09 +0100

    SPRING Configuring repositories for book and author entities.

---
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorsRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5webapp.repositories;
+
+import guru.springframework.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorsRepository extends CrudRepository<Author, Long> {
+
+
+}
